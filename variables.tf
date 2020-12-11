c18a61a6cf6a3019df270f901383d202bdf0d2ba
sshabb697 <38155111+sshabb697@users.noreply.github.com>
2020-12-12 00:28:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,22 @@
-/* 
+
 
 variable "subscription_id" {
   type        = string
-  description = "Your Azure Subscription ID"
+  description = "b40b93a4-0ba5-42f8-a12d-cfa825259239"
 }
 
 variable "client_id" {
   type        = string
-  description = "Your Azure Service Principal appId"
+  description = "48f4baac-e1c7-4862-97dc-9c34ca8f77e5"
 }
 
 variable "client_secret" {
   type        = string
-  description = "Your Azure Service Principal Password"
+  description = "mmyD~LpfMQSi0LJu.OT9p4j0U08oqzCqzi"
 }
 
 variable "tenant_id" {
   type        = string
-  description = "Your Azure Tenant ID"
+  description = "4bbd55ad-91de-4d59-994f-2aa64d5096e2"
 } 
 
-*/
