f35262d3df46f567ebb5ac9ed207e12385c73bad
sshabb697 <38155111+sshabb697@users.noreply.github.com>
2020-12-12 00:34:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 
-
+/*
 variable "subscription_id" {
   type        = string
-  description = "b40b93a4-0ba5-42f8-a12d-cfa825259239"
+  description = "your subscription id"
 }
 
 variable "client_id" {
   type        = string
-  description = "48f4baac-e1c7-4862-97dc-9c34ca8f77e5"
+  description = "your sp id"
 }
 
 variable "client_secret" {
   type        = string
-  description = "mmyD~LpfMQSi0LJu.OT9p4j0U08oqzCqzi"
+  description = "client id"
 }
 
 variable "tenant_id" {
   type        = string
-  description = "4bbd55ad-91de-4d59-994f-2aa64d5096e2"
+  description = "teannt id"
 } 
-
+*/
